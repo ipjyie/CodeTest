3cc8398a12a2977d07758e33145cda8dc4c61c35
김지연 <112389243+ipjyie@users.noreply.github.com>
2024-01-24 23:09:24 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.01 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.kt
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.kt
@@ -1,16 +1,6 @@
 class Solution {
     fun solution(arr: IntArray, n: Int): IntArray {
-     var answer = IntArray(arr.size)
-
-        for (i in 0 until arr.size) {
-            if (arr.size % 2 != 0) {
-                answer[i] = if (i % 2 == 0) arr[i] + n else arr[i]
-            } else {
-                answer[i] = if (i % 2 != 0) arr[i] + n else arr[i]
-            }
-        }
-
-    
+        var answer: IntArray = intArrayOf()
         return answer
     }
 }
