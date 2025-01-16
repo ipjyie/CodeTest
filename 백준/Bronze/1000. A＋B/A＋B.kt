e426d91356d15e25dc72b9b4761746689673a783
김지연 <112389243+ipjyie@users.noreply.github.com>
2025-01-16 20:51:46 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val input = readLine()
+    val parts = input!!.split(" ")
+
+            val num1 = parts[0].toInt() 
+            val num2 = parts[1].toInt() 
+            val sum = num1 + num2
+
+    print(sum)
+}
